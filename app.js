79d0ef1ee408c5011e76f98a19b7bc7ef144d907
Mpergolatti <Mpergolatti@gmail.com>
2022-09-25 16:30:30 -0400

    Arrow function and ES6

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// for (let i = 0; i < profileDataArr.length; i += 1) {
+//   console.log(profileDataArr[i]);
+// }
+// };
+
+// const printProfileData = profileDataArr => {
+
+// profileDataArr.forEach((profileItem) => {
+//   console.log(profileItem)
+// });
+
+// printProfileData(profileDataArgs);
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
+// const printProfileData = profileDataArr => {
+//   profileDataArr.forEach((profileItem) => {
+//     console.log(profileItem)
+//   });
+// };
 
-const printProfileData = (profileDataArr) => {
-  console.log(profileDataArr);
-};
-
-printProfileData(profileDataArr);
+const printProfileData = profileDataArr => {
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+}

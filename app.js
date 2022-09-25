c3582c8125c9e7d0c5aa7d0b28fd13d7d948db82
Mpergolatti <Mpergolatti@gmail.com>
2022-09-25 17:59:03 -0400

    added page-template.js

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-// for (let i = 0; i < profileDataArr.length; i += 1) {
-//   console.log(profileDataArr[i]);
-// }
-// };
-
-// const printProfileData = profileDataArr => {
-
-// profileDataArr.forEach((profileItem) => {
-//   console.log(profileItem)
-// });
-
-// printProfileData(profileDataArgs);
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//   profileDataArr.forEach((profileItem) => {
-//     console.log(profileItem)
-//   });
-// };
-
-const printProfileData = profileDataArr => {
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
